5979cd7f5fb25be51acad15011a39d4c574920d1
Salpodnag <adzusa8@gmail.com>
2025-02-17 20:39:19 +0300

    feat: json_logger

--- cmd/main.go
+++ cmd/main.go
@@ -3,18 +3,31 @@ package main
 import (
 	"Taskie/cfg"
 	"Taskie/db"
+	"Taskie/logger"
 	"fmt"
+	"log/slog"
+	"os"
 )
 
 func main() {
+	lg := logger.New()
+	slog.SetDefault(lg)
+
 	cfg, err := cfg.Load()
 	if err != nil {
-		fmt.Println(err)
+		lg.Error(
+			"failed to load config",
+			slog.String("error", err.Error()),
+		)
 	}
 
 	db, err := db.NewClient(cfg)
 	if err != nil {
-		fmt.Println(err)
+		lg.Error(
+			"failed to connect to database",
+			slog.String("error", err.Error()),
+		)
+		os.Exit(1)
 	}
 	fmt.Println(db)
 }
